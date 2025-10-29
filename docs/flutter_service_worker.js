4756427bfd8b5dd584bf8e33cea1cbe36bbe0ac2
Bruno <brunolobo.14@hotmail.com>
2025-10-29 10:17:15 -0400

    Test version without AWS - should load faster

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "4b9141b583fc234238eabb8bd1787372",
+"flutter_bootstrap.js": "b8926ae30f345e45808e9a87ca5904f7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3d314f32a98a78b18d70a3fe26b0a913",
-"/": "3d314f32a98a78b18d70a3fe26b0a913",
-"main.dart.js": "900d92be23b1c90f363c0f196403667c",
+"index.html": "4f5e764dafdda6b6202e9a38931703ce",
+"/": "4f5e764dafdda6b6202e9a38931703ce",
+"main.dart.js": "855cd169282af9b1d107fa239ee72a59",
 "manifest.json": "febfaef9c4fc1a00e33d53e94cccf5a9",
 "version.json": "bcbd1ed090d9dfa2d92ebd759e678a33"};
 // The application shell files that are downloaded before a service worker can
